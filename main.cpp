d26157ce07cc6f9c0938b2d1fa38ed4e3e1d3593
Dcourt9712 <dacourtwright@csuchico.edu>
2023-09-20 11:26:19 -0700

    resolved #1

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+using namespace std;
 
 const int RGB_HEX_LENGTH = 7;
+ const char allowed_list [22] = ['A', 'a', 'b','B','C','c','D','d','E','e','F','f','0','1','2','3','4','5','6','7','8','9'];
 
 int main(){
     std::string input;
@@ -12,6 +14,11 @@ int main(){
         if( input.size() != RGB_HEX_LENGTH ){
             std::cout << "Please enter the color in hexadecimal format, starting with # followed by six hex values\n";
         }
+        if(input[0] != '#')
+        {
+            std::cout << "Must start with a # for the color\n";
+        }
+
     }while( input.size() != RGB_HEX_LENGTH );
 
     std::cout << "Your hex color is: " << input << std::endl;
